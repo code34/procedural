df6a988cb0ca6ced7e0510ba0324ed9f9dc9ea34
code34 <nicolas_boiteux@yahoo.fr>
2018-01-13 19:11:16 +0100

    first version 0.1 - world generator

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimun;
+        public int maximun;
+
+        public Count(int min, int max)
+        {
+            minimun = min;
+            maximun = max;
+        }
+    }
+
+    public int width = 10;
+    public int length = 10;
+
+    public Count treeCount = new Count(1,3);
+    public Count rockCount = new Count(5,9);
+    
+    public GameObject[] borders;
+    public GameObject[] floors;
+    public GameObject[] rocks;
+    public GameObject[] trees;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+        for (int x= 1; x < width -1; x++)
+        {
+            for (int y = 1; y < length -1; y++ )
+            {
+                gridPositions.Add(new Vector3(x, y, 0));
+            }
+        }
+    }
+
+    /*
+     * Génère le sol 
+     */
+    void BoardSetUp()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int x = -1; x <= width; x++)
+        {
+            for (int y = -1; y <= length; y++)
+            {
+                GameObject toInstantiate = floors[Random.Range(0, floors.Length)];
+                if (x == -1 || x == width || y == length || y == -1)
+                    toInstantiate = borders[Random.Range(0, borders.Length)];
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, 0f, y), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    Vector3 RandomPosition ()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimun, int maximun)
+    {
+        int objectCount = Random.Range(minimun, maximun + 1);
+
+        for (int i=0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.Euler(-90, 0, 0));
+        }
+    }
+
+    public void SetUpScene()
+    {
+        BoardSetUp();
+        InitialiseList();
+        LayoutObjectAtRandom(trees, treeCount.minimun, treeCount.maximun);
+        LayoutObjectAtRandom(rocks, rockCount.minimun, rockCount.maximun);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetUpScene();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+            boardScript.SetUpScene();
+    }
+}
